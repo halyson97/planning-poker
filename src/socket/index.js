08da3530a75bdd007d09cb9d2bc415da2b779a9c
Halyson Rezende <halyson97@gmail.com>
2022-06-13 20:44:02 -0300

    Test redis without password

--- src/socket/index.js
+++ src/socket/index.js
@@ -22,7 +22,7 @@ console.log({
 const client = redis.createClient({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD
+    // password: process.env.REDIS_PASSWORD,
 });
 client.on('error', (err) => console.log('Redis Client Error', err));
 
